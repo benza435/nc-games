dbacb552a1db4c88d3f9190d25544b5a20d0b6e1
ben <benegan435@gmail.com>
2021-05-26 09:40:03 +0100

    added components import file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Home from "./components/Home";
-import Reviews from "./components/Reviews";
-import ReviewById from "./components/ReviewById";
-import Comments from "./components/Comments";
-import Footer from "./components/Footer";
+import {
+  Header,
+  Nav,
+  Home,
+  Reviews,
+  ReviewById,
+  Comments,
+  Footer,
+} from "./components/All";
+
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
---
+++ src/components/All.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import Reviews from "./Reviews";
+import ReviewById from "./ReviewById";
+import Comments from "./Comments";
+import Footer from "./Footer";
+
+export { Header, Nav, Home, Reviews, ReviewById, Comments, Footer };
